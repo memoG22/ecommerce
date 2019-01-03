28fed9b1d8980d96f6f8bbbe8a29d2aade824e97
Memo Armenta <guillermoarmenta520@yahoo.com>
2019-01-03 15:56:43 -0800

    credit card

--- src/Components/ShoppingBasket.js
+++ src/Components/ShoppingBasket.js
@@ -5,6 +5,11 @@ import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
 
 function ShoppingBasket(props) {
   const [items, setItems] = React.useState([]);
+  const [creditCard, setCreditCard] = React.useState("");
+  const [firstname, setFirstname] = React.useState("");
+  const [lastname, setLastname] = React.useState("");
+
+  const [isOpen, toggleModal] = React.useState(false);
 
   React.useEffect(() => {
     appendItems();
@@ -20,6 +25,17 @@ function ShoppingBasket(props) {
       <div className="row">
         <div className="col sm-4">
           <h1 className={Styles.textCenter}>Your Items</h1>
+        </div>
+      </div>
+      <div>
+        <div>
+          <Button
+            style={{ marginLeft: "90%" }}
+            onClick={() => toggleModal(!isOpen)}
+            color="primary"
+          >
+            Check out
+          </Button>
         </div>
       </div>
       <div clasname="row">
@@ -52,6 +68,51 @@ function ShoppingBasket(props) {
             </div>
           </div>
         ))}
+      </div>
+      <div>
+        <Modal isOpen={isOpen}>
+          <Button
+            style={{ backgroundColor: "red" }}
+            onClick={() => toggleModal(!isOpen)}
+          >
+            X
+          </Button>
+          <ModalBody>
+            Fistname
+            <br />
+            <input
+              type="text"
+              placeholder="Firstname"
+              value={firstname}
+              onChange={e => setFirstname(e.target.value)}
+            />
+            <br />
+            Lastname
+            <br />
+            <input
+              type="text"
+              placeholder="Lastname"
+              value={lastname}
+              onChange={e => setLastname(e.target.value)}
+            />
+            <br />
+            Creit card info
+            <br />
+            <input
+              type="number"
+              placeholder="credit card number"
+              value={creditCard}
+              onChange={e => setCreditCard(e.target.value)}
+            />
+            <br />
+            <Button
+              style={{ backgroundColor: "primary" }}
+              onClick={() => toggleModal(!isOpen)}
+            >
+              Buy
+            </Button>
+          </ModalBody>
+        </Modal>
       </div>
     </React.Fragment>
   );
